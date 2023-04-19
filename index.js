fd072e91fab2238d9f95da6ecdac7b6f57891f95
Abraham Richter <arichterbuilds@gmail.com>
2023-04-19 15:57:19 -0400

    finished MVP

--- index.js
+++ index.js
@@ -15,9 +15,28 @@
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
-
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+  this.stomach = [];
 }
+Person.prototype.eat = function(edible) {
+  if(this.stomach.length < 10){
+    this.stomach.push(edible);
+  }
+}
+Person.prototype.poop = function (){
+  this.stomach = [];
+}
+Person.prototype.toString = function(){
+  return `${this.name}, ${this.age}`
+}
+// const mary = new Person('Mary', 50);
+// console.log(mary.toString());
+// mary.eat('pizza');
+// console.log(mary.stomach);
+// mary.poop();
+// console.log(mary.stomach)
 
 
 /*
@@ -36,8 +55,25 @@ function Person() {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
-
+function Car(model, mpg) {
+  this.model = model;
+  this.milesPerGallon = mpg;
+  this.tank = 0;
+  this.odometer = 0;
+}
+Car.prototype.fill = function(gallons){
+  this.tank = this.tank + gallons
+}
+Car.prototype.drive = function(distance){
+  const maxMiles = this.tank * this.milesPerGallon;
+  if (distance <= maxMiles){
+    this.odometer = this.odometer + distance;
+    this.tank = this.tank - (distance / this.milesPerGallon);
+  } else {
+    this.odometer = this.odometer + maxMiles;
+    this.tank = 0;
+    return `I ran out of fuel at ${this.odometer} miles`
+  }
 }
 
 
@@ -49,18 +85,22 @@ function Car() {
         + Should return a string "Playing with x", x being the favorite toy.
 */
 
-function Baby() {
-
+function Baby(name, age, favoriteToy) {
+  Person.call(this, name, age);
+  this.favoriteToy = favoriteToy;
 }
-
+Baby.prototype = Object.create(Person.prototype);
+Baby.prototype.play = function(){
+  return `Playing with ${this.favoriteToy}`;
+}
 
 /* 
   TASK 4
   In your own words explain the four principles for the "this" keyword below:
-  1. 
-  2. 
-  3. 
-  4. 
+  1. Window Binding: when the other rules don't apply, it will return the window, global object, or undefined.
+  2. Implicit Binding: when a function is invoked to look to the left of the dot
+  3. Explicit Binding: .call, .apply, .bind
+  4. New Binding: a function that is a constructor which points to the newly created object
 */
 
 ///////// END OF CHALLENGE /////////
